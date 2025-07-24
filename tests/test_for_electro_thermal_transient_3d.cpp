edd8a608719f8367a8ee93075ce69e0252668877
catyugu <141992786+catyugu@users.noreply.github.com>
2025-07-24 23:27:48 +0800

    refactor: 调整电热瞬态问题的迭代求解器容忍度
    - 将迭代求解器的容忍度从1e-6 修改为 1e-5
    - 这个修改可能会影响计算精度和收敛速度

--- tests/test_for_electro_thermal_transient_3d.cpp
+++ tests/test_for_electro_thermal_transient_3d.cpp
@@ -39,7 +39,7 @@ protected:
         problem->getCouplingManager().addCoupling(std::make_unique<Core::ElectroThermalCoupling>());
 
         problem->setTimeSteppingParameters(0.1, 1.0);
-        problem->setIterativeSolverParameters(30, 1e-6);
+        problem->setIterativeSolverParameters(30, 1e-5);
 
         problem->setup();
     }
